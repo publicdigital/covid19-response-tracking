a6ce4c523cbf47f2a9bc77e3cd263ab26ee9c3bc
James Stewart (VPS) <james@jystewart.net>
2020-05-27 14:40:03 +0000

    Update lighthouse script to read CSV

--- scripts/lighthouse.sh
+++ scripts/lighthouse.sh
@@ -2,7 +2,11 @@ SCRIPT_PATH=`realpath $0`
 SCRIPT_FOLDER=`dirname $SCRIPT_PATH`
 cd $SCRIPT_FOLDER/../lighthouse-reports
 
-while read line; do
-	echo $line
-	/usr/local/bin/lighthouse --output json --output html --chrome-flags="--headless" $line
-done < $SCRIPT_FOLDER/../list.txt
+INPUT=$SCRIPT_FOLDER/../list.csv
+OLDIFS=$IFS
+IFS=','
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read url government etc
+do
+	/usr/local/bin/lighthouse --output json --output html --chrome-flags="--headless" $url
+done < $INPUT
