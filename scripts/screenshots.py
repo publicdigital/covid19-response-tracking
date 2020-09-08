006fc640ace0dd280ba3b3fb8d2df1ce6d46ad1e
James Stewart (VPS) <james@jystewart.net>
2020-09-08 09:57:34 +0000

    Process each URL with a timeout to stop hanging
    The script was getting stuck on some servers that didn't return.
    15s may be too aggressive but we'll see.

--- scripts/screenshots.py
+++ scripts/screenshots.py
@@ -7,20 +7,29 @@ import os
 formatted_date = datetime.now().strftime("%Y-%m-%d")
 directories = c19utils.establish_directories(formatted_date)
 
+async def fetch_url(url):
+  try:
+    print("Processing " + url)
+    filename = c19utils.filter_bad_filename_chars(url)
+    output_filename = os.path.join(directories['today'], ('%s.png' % filename))
+
+    browser = await pyppeteer.launch()
+    page = await browser.newPage()
+    await page.setViewport({'width': 800, 'height': 1280})
+    await page.goto(url)
+    await page.screenshot({'path': output_filename})
+    await browser.close()
+  except pyppeteer.errors.TimeoutError:
+    print("Timed out fetching " + url)
+  except pyppeteer.errors.PageError:
+    print("Page error for " + url)
+
 async def main():
   for url in c19utils.CovidURLList():
     try:
-      print("Processing " + url)
-      filename = c19utils.filter_bad_filename_chars(url)
-      output_filename = os.path.join(directories['today'], ('%s.png' % filename))
+      await asyncio.wait_for(fetch_url(url), timeout=15.0)
+    except asyncio.TimeoutError:
+      print("asyncio timeout for " + url)
 
-      browser = await pyppeteer.launch()
-      page = await browser.newPage()
-      await page.setViewport({'width': 800, 'height': 1280})
-      await page.goto(url)
-      await page.screenshot({'path': output_filename})
-      await browser.close()
-    except pyppeteer.errors.TimeoutError:
-      print("Timed out fetching " + url)
 
 asyncio.get_event_loop().run_until_complete(main())
